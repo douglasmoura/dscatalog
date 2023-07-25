48f69597399ca2a9c7a55c5e40f2f88f728eea53
Douglas Moura <douglasmoura2@outlook.com>
2023-07-25 17:09:27 -0300

    H2 database

--- src/main/java/com/example/dscatalog/entities/Category.java
+++ src/main/java/com/example/dscatalog/entities/Category.java
@@ -2,8 +2,18 @@ package com.example.dscatalog.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
